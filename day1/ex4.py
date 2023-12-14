b4b0b700aa22002e39f37a0e562aa07baf443263
Ujjwal Puri <ujjwalpuri221@gmail.com>
2023-12-14 16:27:05 +0545

    python arithmetics

---
+++ day1/ex4.py
@@ -0,0 +1,16 @@
+#basics
+
+a=4
+b=5
+#exponent
+print(a**b)
+#modulus
+print(b%a)
+#floored quotient
+print(b//a)
+print('If the quotient comes in a dot value, then it floors it')
+
+print('\ndivision:\n')
+print((b/a))
+print('that is the answer of multiplicaton\n',(b*a))
+#bodmas rule is followed just like mathematics
