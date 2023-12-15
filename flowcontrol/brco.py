575100ab2c4748064b6f7266bd8f93e2dedbb862
Ujjwal Puri <ujjwalpuri221@gmail.com>
2023-12-15 18:33:45 +0545

    additonal truth false type

---
+++ flowcontrol/brco.py
@@ -0,0 +1,11 @@
+while True:
+    print('Who are you')
+    name=input()
+    if name!='joe':
+        continue
+    #the loop ended here
+    print('password:')
+    password=input()
+    if password=='swordfish':
+        break
+print('Acess granted')
---
+++ flowcontrol/tf.py
@@ -0,0 +1,12 @@
+name=''
+while not name: 
+    print('Enter your name:')
+    name=input()
+print('How many guests will you have?')
+numofGuests=int(input())
+if numofGuests:
+    print("room")
+print('dont')
+
+#an empty screen is considered to be falsey value
+
