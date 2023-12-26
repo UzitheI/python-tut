c4d1f25b93236558976f320f55026880a0818f21
Ujjwal Puri <ujjwalpuri221@gmail.com>
2023-12-26 15:17:36 +0545

    in not in

---
+++ lists/innotin.py
@@ -0,0 +1,10 @@
+names=['jeremy','joue','samber']
+
+print('Enter your names:')
+name=input()
+
+if name not in names:
+    print('The person is not in our list')
+else:
+    print('The person is in our list')
+
