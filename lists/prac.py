9d294acccef5133f5a2c976fc22f4d1cf525b3a7
Ujjwal Puri <ujjwalpuri221@gmail.com>
2023-12-26 15:13:20 +0545

    basic practice

---
+++ lists/prac.py
@@ -0,0 +1,6 @@
+names=['james','micah','rooney']
+
+for i in range(len(names)):
+    print('The first is number '+str(i+1)+' and his name is :'+str(names[i]))
+
+#this is normally how the lists are used
