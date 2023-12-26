8f2f2e87f09823a5d63a4a3adc089ba46a9e3ab2
Ujjwal Puri <ujjwalpuri221@gmail.com>
2023-12-26 14:32:43 +0545

    lists

---
+++ lists/basics.py
@@ -0,0 +1,18 @@
+marks=[4,5,6,2,3,4]
+
+print('Anish scored '+str(marks[4])+' in his test')
+
+#storing lists withing lists
+
+store=[[5,3,2],[3,2,3],4,3]
+
+print(str(store[0]))
+
+#also i can acces the element inside that list
+
+print(str(store[0][2]))
+
+#there are negative indexes as well and they start from -1, here in the above list -1 means 3
+
+
+print(str(store[-2]))
