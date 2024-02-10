12a116217deebfa3ca431bdab40be0bda4667080
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-02-10 22:25:30 +0545

    what are dictionaries

---
+++ Dictionaries/ex1.py
@@ -0,0 +1,12 @@
+#dictionaries help accessing data much easier
+
+mycat={'name':'samantha','color':'grey'}
+
+print(mycat['name'])
+
+#one of the primary differences in lists and dictionaries is that the elements inside dictionaries are not ordered 
+
+#when you compare a list with another list then the output will be false if done inside an interpreter shell 
+
+#when you do that with a dictionary the value is returned to be true
+
