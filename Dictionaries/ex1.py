f9d1085ab93450fdbb19a6faa4ff84fa9bfb01fb
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-02-10 22:56:18 +0545

    ex1

--- Dictionaries/ex1.py
+++ Dictionaries/ex1.py
@@ -8,5 +8,8 @@ print(mycat['name'])
 
 #when you compare a list with another list then the output will be false if done inside an interpreter shell 
 
-#when you do that with a dictionary the value is returned to be true
+#when you do that with a dictionary the value is returned to be true because there are seemingly no order
 
+#while from python 3.7 dictionaries can remember the sequence by which elements were put in the dictionary, it is still not the best to use this because older versions of python dont use them, moreover there is no surefire way to access them as they dont have indexes like lists
+
+#every dictionary has a key and a value and we can even use them in the lists
