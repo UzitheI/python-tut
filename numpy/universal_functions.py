4298cdbfe72703c15d2524a4bca64fe5abfc54ed
UzitheI <ujjwalpuri221@gmail.com>
2024-07-14 04:42:39 +0545

    using universal functions of numpy

---
+++ numpy/universal_functions.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+#numpy universal function
+
+np1= np.array([-1,-3,0,1,2,3,4,5,6,7,8,9])
+print(np1)
+
+#square root of each element
+
+# print(np.sqrt(np1))
+
+#absolute value of our function
+
+# print(np.absolute(np1))
+
+#exponents
+
+# print(np.exp(np1))
+
+#min/max
+
+print(np.min(np1))
+
+#sign positive or negative
+
+print(np.sign(np1))
+#prints -1 for negative numbers, 0 for 0 and 1 for positive numbers
+
+#trig sin cos log
+
+# print(np.log(np1))
+
