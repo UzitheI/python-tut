bd1ab4d42914e02788bc766dc96b8ca00a1f88bb
UzitheI <ujjwalpuri221@gmail.com>
2025-01-01 23:46:27 +0545

    tried some math functions

--- interview_prep/basics.py
+++ interview_prep/basics.py
@@ -55,3 +55,59 @@ print(5/2) # yields 2.5
 #if integer division is needed 
 
 print(int(5/2))
+
+
+#also other languages when negative division round of to number closer to 0 but python rounds of towards negative infinty
+
+print(-7/2) #yields 3.5 because decimal division
+
+#now integer division
+
+print(int(-7/2))
+
+#if you want to round down
+
+print(5//2) #this rounds to 2
+
+#remainder is calculated by %
+
+print(10%3)
+
+#again the issue is remainder with negative values
+
+print(-10%3) #maybe expecting -1 but we get 2, which is differnt from other C based languages
+
+#to get actual value, we need to import math
+
+import math
+
+print("The actual correct mod value is ",math.fmod(10,3))
+
+
+#to round down
+
+print("Round down this number ", math.floor(10/3))
+
+#to round up
+
+print("Round up is done by ",math.ceil(10/3))
+
+#to square
+
+print("Square is taken by ", math.sqrt(5))
+
+#to power
+
+print("power is given by", math.pow(2,5))
+
+#python numbers are infinite so they never overflow, which means
+
+print(" A large number ", math.pow(2,300))
+
+#to get the most minimum value
+
+float("-inf")
+
+#to get max
+
+print((math.pow(2,300))< float("inf")) # should print true
