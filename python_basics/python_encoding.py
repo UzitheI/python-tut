ec8f044a094f6558fd0433431fa2b292f1c71276
UzitheI <ujjwalpuri221@gmail.com>
2025-01-07 23:43:05 +0545

    python encoding

---
+++ python_basics/python_encoding.py
@@ -0,0 +1,5 @@
+# -*- coding: cp1252 -*-
+
+#this ensures that the encoding is done in cp1252 format
+#by default a python file is derived as UTF-8 which means that if we are using a different encoding that the default we must use a header#
+
