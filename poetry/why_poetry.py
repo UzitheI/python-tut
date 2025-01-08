da75cfbeddec1c45c8cbd003e0a3995415beff45
UzitheI <ujjwalpuri221@gmail.com>
2025-01-08 22:10:28 +0545

    poetry info

--- poetry/why_poetry.py
+++ poetry/why_poetry.py
@@ -1,1 +1,2 @@
-#poetry is a package that helps you maintain your virtual environments, it is also responsibel for 
+#poetry is a package that helps you maintain your virtual environments, it is also responsible for
+
